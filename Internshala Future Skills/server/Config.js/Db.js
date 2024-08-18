9871a91de25b0365036a3c546929f55a345ce72d
heyNazim <nazim.saifi1908@gmail.com>
2024-08-18 12:44:09 +0530

    tytyty

--- Internshala Future Skills/server/Config.js/Db.js
+++ Internshala Future Skills/server/Config.js/Db.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 const connectDb =async ()=>{
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL)
+            const conn = await mongoose.connect(process.env.MONGO_URL)
         console.log(`MongoDB Database Connected: ${conn.connection.host}`.bgGreen)
     } catch (error) {
         console.log(`Something went wrong in Database: ${error}`.bgRed)
@@ -13,3 +13,7 @@ const connectDb =async ()=>{
 
 
 export default connectDb;
+
+
+
+PORT = 8080
